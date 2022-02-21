6bffc1d386b0018360c12a0a0954e16184ca2a3e
jeongminsu <ansuz0616@naver.com>
2022-02-21 16:17:51 +0900

    댓글 기능
     구현을 하려 했으나 과제에 없어서 구현 보류

---
+++ router/comment.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { Op } = require('sequelize');
+const { comments, likes } = require('../models');
+
+const routes = express.Router();
+
+// 댓글 추가
+
+// 댓글 수정
+
+// 댓글 삭제
+
+
+module.exports = routes;
