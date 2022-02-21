326a6dbd81e23fe6d7839f4b17ae48ee60ca9561
jeongminsu <ansuz0616@naver.com>
2022-02-21 16:13:41 +0900

    Feat: app.js 추가

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const auth = require('./middleWares/auth-middleware');
+const requset = require('./middleWares/requset-Middleware');
+const cors = require('./middleWares/cors');
+const app = express();
+
+const port = 8080;
+
+const userRouter = require('./router/user');
+const postRouter = require('./router/post');
+const commentRouter = require('./router/comment');
+
+// app.use(cors());
+app.use(express.json());
+// app.use();
+app.use('/api', [userRouter, postRouter, commentRouter]);
+
+app.listen(port, () => {
+  console.log(port+'서버가 켜졌습니다.');
+})
