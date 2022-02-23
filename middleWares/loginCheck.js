39f5904d9a4edda80df940c95045f51f03ea3cb9
jeongminsu <ansuz0616@naver.com>
2022-02-23 20:36:48 +0900

    수정
    user 정보가 안받아지는 버그 수정

--- middleWares/loginCheck.js
+++ middleWares/loginCheck.js
@@ -6,7 +6,7 @@ const secretKey = require('../config/secretkey').secretKey;
 user와 boolean값을 반환.
 로그인을 하지 않아도 사용할 수 있어야 하는 기능을 사용할 때 사용.
 */
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   // console.log('미들웨어 check');
   const { cookie } = req.cookies;
   // console.log(cookie.token);
@@ -18,7 +18,8 @@ module.exports = (req, res, next) => {
 
   const { userId } = jwt.verify(cookie.token, secretKey);
   // console.log(userId);
-  const user = users.findOne({where: {id: userId}});
+  const user = await users.findOne({where: {id: userId}});
+  // console.log(user)
   res.locals.user = user;
   res.locals.boolean = true;
 
