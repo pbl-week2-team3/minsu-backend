00aa189de5edf09dedaff2303b42fa1cde289f94
jeongminsu <ansuz0616@naver.com>
2022-02-28 00:43:37 +0900

    추가
    test code 구현시 필요한 변수 저장

---
+++ val.js
@@ -0,0 +1,72 @@
+let date1 = new Date("2022-02-26T15:47:38.000Z");
+let date2 = new Date("2022-02-26T15:49:50.000Z");
+
+exports.p = [{
+  id: 1,
+  nickname: "minsu",
+  contents: "테스트",
+  profile_img: "https://media.discordapp.net/attachments/769096782088503298/947087079056212038/1.png",
+  img_url: "aa",
+  like_count: 0,
+  like_check: false,
+  me: false,
+  reg_date: date1,
+},
+{
+  id: 2,
+  nickname: "minsu",
+  contents: "테스트2",
+  profile_img: "https://media.discordapp.net/attachments/769096782088503298/947087079056212038/1.png",
+  img_url: "aaaa",
+  like_count: 0,
+  like_check: false,
+  me: false,
+  reg_date: date2,
+}];
+
+exports.p2 = [{
+  id: 1,
+  nickname: "minsu",
+  contents: "테스트",
+  profile_img: "https://media.discordapp.net/attachments/769096782088503298/947087079056212038/1.png",
+  img_url: "aa",
+  like_count: 0,
+  like_check: false,
+  me: true,
+  reg_date: date1,
+},
+{
+  id: 2,
+  nickname: "minsu",
+  contents: "테스트2",
+  profile_img: "https://media.discordapp.net/attachments/769096782088503298/947087079056212038/1.png",
+  img_url: "aaaa",
+  like_count: 0,
+  like_check: false,
+  me: true,
+  reg_date: date2,
+}];
+
+exports.p3 = {
+  id: 1,
+  nickname: "minsu",
+  contents: "테스트",
+  profile_img: "https://media.discordapp.net/attachments/769096782088503298/947087079056212038/1.png",
+  img_url: "aa",
+  like_count: 0,
+  like_check: false,
+  me: false,
+  reg_date: date1,
+};
+
+exports.p4 = {
+  id: 1,
+  nickname: "minsu",
+  contents: "테스트",
+  profile_img: "https://media.discordapp.net/attachments/769096782088503298/947087079056212038/1.png",
+  img_url: "aa",
+  like_count: 0,
+  like_check: false,
+  me: true,
+  reg_date: date1,
+};
