8f692e228386723bba01e1da84d2ad9f5f728764
jeongminsu <ansuz0616@naver.com>
2022-02-28 00:42:34 +0900

    수정
    코드의 간소화

--- router/user.js
+++ router/user.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const { jsonp } = require('express/lib/response');
 const joi = require('joi');
 const { Op } = require('sequelize');
 const jwt = require('jsonwebtoken');
 
 const { users } = require('../models');
 const loginCheck = require('../middleWares/loginCheck');
-const secretKey = require('../config/secretkey').secretKey;
-const option = require('../config/secretkey').option;
+const { secretKey, option } = require('../config/secretkey');
 
 // { "user_id":"aaa", "contents":"되나?", "img_url":"링크" }
 
@@ -31,42 +29,53 @@ routes.post('/register', loginCheck, async (req, res) => {
     });
     return;
   }
-  let { id, nickname, password, confirmPassword, profile_img_url } = await registerSchema.validateAsync(req.body);
 
-  // console.log(id, nickname, password, confirmPassword, profile_img_url);
-  if (password.includes(nickname)) {
+  try {
+    let { id, nickname, password, confirmPassword, profile_img_url } = await registerSchema.validateAsync(req.body);
+
+    // console.log(id, nickname, password, confirmPassword, profile_img_url);
+    if (password.includes(nickname)) {
+      res.status(401).json({
+        success: false,
+        message: '사용할 수 없는 비밀번호입니다.\n닉네임과 겹치지 않게 만들어주세요.'
+      });
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      res.status(401).json({
+        messages: "비밀번호가 일치하지 않습니다."
+      });
+      return;
+    }
+
+    const findUser = await users.findOne({ where: { [Op.or]: [{ id }, { nickname }] } });
+    // console.log(findUsers);
+    if (findUser) {
+      res.status(401).json({
+        messages: '아이디 또는 닉네임이 중복됩니다.',
+      });
+      return;
+    }
+
+    if (profile_img_url === ' ' || profile_img_url === null || profile_img_url === undefined) {
+      profile_img_url = 'https://media.discordapp.net/attachments/769096782088503298/945677346525040690/default.png';
+    }
+
+    await users.create({ id, nickname, password, profile_img_url });
+    res.status(201).json({
+      success: true,
+      messages: '회원가입 성공!',
+    });
+  } catch (error) {
+    // console.log(error);
     res.status(401).json({
       success: false,
-      message: '사용할 수 없는 비밀번호입니다.\n닉네임과 겹치지 않게 만들어주세요.'
+      messages: '다시 보내세요.',
     });
     return;
   }
 
-  if (password !== confirmPassword) {
-    res.status(401).json({
-      messages: "비밀번호가 일치하지 않습니다."
-    });
-    return;
-  }
-
-  const findUser = await users.findOne({ where: {[Op.or]: [{id},{nickname}]}});
-  // console.log(findUsers);
-  if (findUser) {
-    res.status(401).json({
-      messages: '아이디 또는 닉네임이 중복됩니다.',
-    });
-    return;
-  }
-
-  if (profile_img_url === ' ' || profile_img_url === null || profile_img_url === undefined) {
-    profile_img_url = 'https://media.discordapp.net/attachments/769096782088503298/945677346525040690/default.png';
-  }
-
-  await users.create({ id, nickname, password, profile_img_url });
-  res.status(201).json({
-    success: true,
-    messages: '회원가입 성공!',
-  });
 });
 
 const loginSchema = joi.object({
@@ -78,8 +87,8 @@ const loginSchema = joi.object({
 routes.post('/login', loginCheck, async (req, res) => {
   // console.log(res.locals.boolean);
   if (res.locals.boolean) {
-    res.status(200).json({
-      success: true,
+    res.status(401).json({
+      success: false,
       message: '이미 로그인이 되어있습니다.'
     });
     return;
@@ -89,44 +98,52 @@ routes.post('/login', loginCheck, async (req, res) => {
     const { id, password } = await loginSchema.validateAsync(req.body);
 
     const findUser = await users.findByPk(id);
-    // console.log(findUser.id, key);
+    // console.log(findUser.id, secretKey);
 
     if (findUser && findUser.password === password) {
-      const token = jwt.sign({ userId: findUser.id }, secretKey, option);
-      // console.log(jwt.verify(token, key));
-      const cookieValue = {
-        token,
-        nickname: findUser.nickname,
-      }
-      res.cookie('cookie', cookieValue, {
-        path: '/',
-        maxAge: 3600000,
-      });
+      const token = jwt.sign({ user_id: findUser.id, nickname: findUser.nickname, profile_img_url: findUser.profile_img_url }, secretKey, option);
+      // console.log(jwt.verify(token, secretKey));
       res.status(201).json({
         success: true,
         messages: '로그인 성공!',
         token,
       });
     } else {
-      res.status(200).json({
+      res.status(401).json({
         success: false,
         messages: '아이디 또는 비밀번호가 틀렸습니다.',
-        token,
       });
     }
 
   } catch (error) {
-    console.log(error);
-    res.status(401).json({});
+    // console.log(error);
+    res.status(401).json({
+      success: false,
+      messages: '잘못된 값을 입력하셨습니다. \n빈칸이 있는지 확인해 주세요.'
+    });
   }
 });
 
-routes.get('/logout', (req, res) => {
-  res.clearCookie('cookie');
+routes.get('/me', (req, res) => {
+  const { token } = req.headers;
+  const decode = jwt.verify(token, secretKey);
+  const user = users.findOne({ where: { id: decode.user_id } });
+  // console.log(user);
+  const reToken = jwt.sign({ user_id: user.id, nickname: user.nickname, profile_img_url: user.profile_img_url }, secretKey, option);
+
   res.status(200).json({
     success: true,
-    messages: '로그아웃',
+    messages: '내정보를 성공적으로 받았습니다.',
+    token: reToken,
   });
-})
+});
+
+const adminSchema = joi.object({
+  id: joi.string().required(),
+  nickname: joi.string().required(),
+  password: joi.string().required(),
+  confirmPassword: joi.string(),
+  profile_img_url: joi.string(),
+});
 
 module.exports = routes;
